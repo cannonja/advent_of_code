d3d7231a0c2ca9183439f25a939379747344251d
Jack Cannon <jack_cannon@live.com>
2022-12-16 21:37:53 -0800

    Continued day7

--- day7/day7.py
+++ day7/day7.py
@@ -1,3 +1,63 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DirSum:
+    value: int = 0
+
+
+
+class Node:
+    def __init__(self, name, size=0, is_dir=False):
+        self.name = name
+        self._size = size
+        self.is_dir = is_dir
+        self.children = []
+
+    
+    def add_child(self, node):
+        self.children.append(node)
+
+
+    def calculate_size(self):
+        if self.is_dir:
+            for child in self.children:
+                self._size += child.calculate_size()
+
+        return self._size
+
+
+    def sum_large_dirs(self, dir_sum):
+        if self.is_dir:
+            for child in self.children:
+                self._size += child.sum_large_dirs(dir_sum)
+            if self._size <= 100000:
+                dir_sum.value += self._size
+
+
+    @property
+    def size(self):
+        return self._size
+
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+
+
+    def __repr__(self):
+        return f"name = {self.name}, size = {self._size}, nchildren = {len(self.children)}"
+
+
+class ElfFS:
+    def __init__(self, root):
+        self.root = root
+
+
+    
+
+
+
 
 
 
